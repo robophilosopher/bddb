3bb07467e9e4c40c57399c5c1913f9fdab3faff7
Anthony Landreth <robophilosopher@gmail.com>
2018-02-10 12:20:56 -0800

    allows Selection to receive a node

--- iterators/selection.py
+++ iterators/selection.py
@@ -2,9 +2,9 @@ from iterators.iterator import Iterator
 from iterators.file_scan import FileScan
 
 class Selection(Iterator):
-    def __init__(self, predicatef):
-        self.inputs = FileScan("movie_test_data.csv")
-
+    def __init__(self, node, predicatef):
+        # self.inputs = FileScan("movie_test_data.csv")
+        self.inputs = node
         self.predicatef = predicatef
 
     def __next__(self):
--- tests/selection_test.py
+++ tests/selection_test.py
@@ -1,13 +1,15 @@
 import unittest
 
 from iterators.selection import Selection
+from iterators.file_scan import FileScan
 
 class TestSelection(unittest.TestCase):
     def test_selection(self):
         predicate = lambda r: 'Toy Story (1995)' in r
         expected = [['1', 'Toy Story (1995)', 'Adventure|Animation|Children|Comedy|Fantasy']]
         results = []
-        s = Selection(predicate)
+        fs = FileScan("movie_test_data.csv")
+        s = Selection(fs, predicate)
 
         for row in s:
             results.append(row)
