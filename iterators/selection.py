1a33964edae75a520440b0b05378eb4b47fd4866
Anthony Landreth <robophilosopher@gmail.com>
2018-02-10 00:44:34 -0800

    hacks out Selection

---
+++ iterators/selection.py
@@ -0,0 +1,19 @@
+from iterators.iterator import Iterator
+from iterators.file_scan import FileScan
+
+class Selection(Iterator):
+    def __init__(self, predicatef):
+        self.inputs = FileScan("movie_test_data.csv")
+
+        self.predicatef = predicatef
+
+    def __next__(self):
+        while True:
+            row = next(self.inputs)
+            if self.predicatef(row):
+                return row
+
+    # Test helper method to make warning go away.
+    # Might be otherwise useless.
+    def close_file(self):
+        self.inputs.close_file()
---
+++ tests/selection_test.py
@@ -0,0 +1,19 @@
+import unittest
+
+from iterators.selection import Selection
+
+class TestSelection(unittest.TestCase):
+    def test_selection(self):
+        predicate = lambda r: 'Toy Story (1995)' in r
+        expected = [['1', 'Toy Story (1995)', 'Adventure|Animation|Children|Comedy|Fantasy']]
+        results = []
+        s = Selection(predicate)
+
+        for row in s:
+            print(row)
+            results.append(row)
+
+        s.close_file()
+
+        assert(results) == expected
+
